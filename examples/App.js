e844bdf36831d2027123da647467c3c4d665730e
AlexKVal <alexkval@gmail.com>
2015-08-05 09:55:26 +0300

    Remove obsolete code

--- examples/App.js
+++ examples/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Button from 'react-bootstrap/lib/Button';
 import Editor from 'component-playground';
-import hyphenate from 'dom-helpers/util/hyphenateStyle';
 
 import PropTable from './PropTable';
 
