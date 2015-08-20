001665affd037dadc42b525cb2160ba60782d1b5
Cesar Andreu <cesarandreu@gmail.com>
2015-08-20 03:19:35 -0400

    Clean up examples webpack config

--- webpack/examples.config.js
+++ webpack/examples.config.js
@@ -4,8 +4,6 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
     path.join(__dirname, '../examples/App.js')
   ],
   output: {
@@ -27,8 +25,7 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.less/, loader: 'style-loader!css-loader!less-loader', exclude: /node_modules/ },
-      { test: /\.js$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/},
-      { test: /\.js$/, loaders: ['babel'], include: path.join(__dirname, '..', 'src')}
+      { test: /\.js$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ }
     ]
   }
 };
