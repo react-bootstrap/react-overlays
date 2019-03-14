86cc426a0d5f43c30b4cbb45b038fc14336c42de
Jimmy Jia <tesrin@gmail.com>
2019-03-14 14:58:54 -0400

    feat: Remove affix support

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import Affix from './Affix'
-import AutoAffix from './AutoAffix'
-import Modal from './Modal'
-import Overlay from './Overlay'
-import Portal from './Portal'
-import RootCloseWrapper from './RootCloseWrapper'
-import Dropdown from './Dropdown'
+import Dropdown from './Dropdown';
+import Modal from './Modal';
+import Overlay from './Overlay';
+import Portal from './Portal';
+import RootCloseWrapper from './RootCloseWrapper';
 
-export { Affix, AutoAffix, Dropdown, Modal, Overlay, Portal, RootCloseWrapper }
+export { Dropdown, Modal, Overlay, Portal, RootCloseWrapper };
--- src/utils/ownerWindow.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom';
-import ownerWindow from 'dom-helpers/ownerWindow';
-
-export default function (componentOrElement) {
-  return ownerWindow(ReactDOM.findDOMNode(componentOrElement));
-}
