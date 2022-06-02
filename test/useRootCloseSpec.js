3b7fb53d4b2141bbffa476608dfa1c0f694fa429
charlesroelli <3193168+charlesroelli@users.noreply.github.com>
2022-06-02 06:15:29 +0200

    feat(useRootClose): add support for open shadow roots (#1004)
    * Support useRootClose inside shadow roots
    
    * Fix lint errors in useRootClose.ts, useRootCloseSpec.js
    
    * Ignore empty composedPath in useRootClose
    
    Co-authored-by: Charles Roelli <charles.roelli@hey.com>

--- test/useRootCloseSpec.js
+++ test/useRootCloseSpec.js
@@ -7,190 +7,220 @@ import { mount } from 'enzyme';
 import useRootClose from '../src/useRootClose';
 
 const escapeKeyCode = 27;
-
-describe('useRootClose', () => {
-  let attachTo;
-
-  beforeEach(() => {
-    attachTo = document.createElement('div');
-    document.body.appendChild(attachTo);
+const configs = [
+  {
+    description: '',
+    useShadowRoot: false,
+  },
+  {
+    description: 'with shadow root',
+    useShadowRoot: true,
+  },
+];
+// Wrap simulant's created event to add composed: true, which is the default
+// for most events.
+const fire = (node, event, params) => {
+  const simulatedEvent = simulant(event, params);
+  const fixedEvent = new simulatedEvent.constructor(simulatedEvent.type, {
+    bubbles: simulatedEvent.bubbles,
+    button: simulatedEvent.button,
+    cancelable: simulatedEvent.cancelable,
+    composed: true,
   });
+  fixedEvent.keyCode = simulatedEvent.keyCode;
+  node.dispatchEvent(fixedEvent);
+  return fixedEvent;
+};
 
-  afterEach(() => {
-    ReactDOM.unmountComponentAtNode(attachTo);
-    document.body.removeChild(attachTo);
-  });
+configs.map((config) =>
+  // eslint-disable-next-line mocha/no-setup-in-describe
+  describe(`useRootClose ${config.description}`, () => {
+    let attachTo, renderRoot, myDiv;
 
-  describe('using default event', () => {
-    // eslint-disable-next-line mocha/no-setup-in-describe
-    shouldCloseOn(undefined, 'click');
-  });
-
-  describe('using click event', () => {
-    // eslint-disable-next-line mocha/no-setup-in-describe
-    shouldCloseOn('click', 'click');
-  });
-
-  describe('using mousedown event', () => {
-    // eslint-disable-next-line mocha/no-setup-in-describe
-    shouldCloseOn('mousedown', 'mousedown');
-  });
-
-  function shouldCloseOn(clickTrigger, eventName) {
-    function Wrapper({ onRootClose, disabled }) {
-      const ref = useRef();
-      useRootClose(ref, onRootClose, {
-        disabled,
-        clickTrigger,
-      });
-
-      return (
-        <div ref={ref} id="my-div">
-          hello there
-        </div>
-      );
-    }
-
-    it('should close when clicked outside', () => {
-      let spy = sinon.spy();
-
-      mount(<Wrapper onRootClose={spy} />, { attachTo });
-
-      simulant.fire(document.getElementById('my-div'), eventName);
-
-      expect(spy).to.not.have.been.called;
-
-      simulant.fire(document.body, eventName);
-
-      expect(spy).to.have.been.calledOnce;
-
-      expect(spy.getCall(0).args[0].type).to.be.oneOf(['click', 'mousedown']);
+    beforeEach(() => {
+      renderRoot = document.createElement('div');
+      if (config.useShadowRoot) {
+        renderRoot.attachShadow({ mode: 'open' });
+      }
+      document.body.appendChild(renderRoot);
+      attachTo = config.useShadowRoot ? renderRoot.shadowRoot : renderRoot;
+      myDiv = () => attachTo.querySelector('#my-div');
     });
 
-    it('should not close when right-clicked outside', () => {
-      let spy = sinon.spy();
-      mount(<Wrapper onRootClose={spy} />, { attachTo });
-
-      simulant.fire(document.getElementById('my-div'), eventName, {
-        button: 1,
-      });
-
-      expect(spy).to.not.have.been.called;
-
-      simulant.fire(document.body, eventName, { button: 1 });
-
-      expect(spy).to.not.have.been.called;
+    afterEach(() => {
+      ReactDOM.unmountComponentAtNode(renderRoot);
+      document.body.removeChild(renderRoot);
     });
 
-    it('should not close when disabled', () => {
-      let spy = sinon.spy();
-      mount(<Wrapper onRootClose={spy} disabled />, { attachTo });
-
-      simulant.fire(document.getElementById('my-div'), eventName);
-
-      expect(spy).to.not.have.been.called;
-
-      simulant.fire(document.body, eventName);
-
-      expect(spy).to.not.have.been.called;
+    describe('using default event', () => {
+      // eslint-disable-next-line mocha/no-setup-in-describe
+      shouldCloseOn(undefined, 'click');
     });
 
-    it('should close when inside another RootCloseWrapper', () => {
-      let outerSpy = sinon.spy();
-      let innerSpy = sinon.spy();
+    describe('using click event', () => {
+      // eslint-disable-next-line mocha/no-setup-in-describe
+      shouldCloseOn('click', 'click');
+    });
 
-      function Inner() {
+    describe('using mousedown event', () => {
+      // eslint-disable-next-line mocha/no-setup-in-describe
+      shouldCloseOn('mousedown', 'mousedown');
+    });
+
+    function shouldCloseOn(clickTrigger, eventName) {
+      function Wrapper({ onRootClose, disabled }) {
         const ref = useRef();
-        useRootClose(ref, innerSpy, { clickTrigger });
+        useRootClose(ref, onRootClose, {
+          disabled,
+          clickTrigger,
+        });
 
         return (
-          <div ref={ref} id="my-other-div">
+          <div ref={ref} id="my-div">
             hello there
           </div>
         );
       }
 
-      function Outer() {
+      it('should close when clicked outside', () => {
+        let spy = sinon.spy();
+
+        mount(<Wrapper onRootClose={spy} />, { attachTo });
+
+        fire(myDiv(), eventName);
+
+        expect(spy).to.not.have.been.called;
+
+        fire(document.body, eventName);
+
+        expect(spy).to.have.been.calledOnce;
+
+        expect(spy.getCall(0).args[0].type).to.be.oneOf(['click', 'mousedown']);
+      });
+
+      it('should not close when right-clicked outside', () => {
+        let spy = sinon.spy();
+        mount(<Wrapper onRootClose={spy} />, { attachTo });
+
+        fire(myDiv(), eventName, { button: 1 });
+
+        expect(spy).to.not.have.been.called;
+
+        fire(document.body, eventName, { button: 1 });
+
+        expect(spy).to.not.have.been.called;
+      });
+
+      it('should not close when disabled', () => {
+        let spy = sinon.spy();
+        mount(<Wrapper onRootClose={spy} disabled />, { attachTo });
+
+        fire(myDiv(), eventName);
+
+        expect(spy).to.not.have.been.called;
+
+        fire(document.body, eventName);
+
+        expect(spy).to.not.have.been.called;
+      });
+
+      it('should close when inside another RootCloseWrapper', () => {
+        let outerSpy = sinon.spy();
+        let innerSpy = sinon.spy();
+
+        function Inner() {
+          const ref = useRef();
+          useRootClose(ref, innerSpy, { clickTrigger });
+
+          return (
+            <div ref={ref} id="my-other-div">
+              hello there
+            </div>
+          );
+        }
+
+        function Outer() {
+          const ref = useRef();
+          useRootClose(ref, outerSpy, { clickTrigger });
+
+          return (
+            <div ref={ref}>
+              <div id="my-div">hello there</div>
+              <Inner />
+            </div>
+          );
+        }
+
+        mount(<Outer />, { attachTo });
+
+        fire(myDiv(), eventName);
+
+        expect(outerSpy).to.have.not.been.called;
+        expect(innerSpy).to.have.been.calledOnce;
+
+        expect(innerSpy.getCall(0).args[0].type).to.be.oneOf([
+          'click',
+          'mousedown',
+        ]);
+      });
+    }
+
+    describe('using keyup event', () => {
+      function Wrapper({ children, onRootClose, event: clickTrigger }) {
         const ref = useRef();
-        useRootClose(ref, outerSpy, { clickTrigger });
+        useRootClose(ref, onRootClose, { clickTrigger });
 
         return (
-          <div ref={ref}>
-            <div id="my-div">hello there</div>
-            <Inner />
+          <div ref={ref} id="my-div">
+            {children}
           </div>
         );
       }
 
-      mount(<Outer />, { attachTo });
-
-      simulant.fire(document.getElementById('my-div'), eventName);
-
-      expect(outerSpy).to.have.not.been.called;
-      expect(innerSpy).to.have.been.calledOnce;
-
-      expect(innerSpy.getCall(0).args[0].type).to.be.oneOf([
-        'click',
-        'mousedown',
-      ]);
-    });
-  }
-
-  describe('using keyup event', () => {
-    function Wrapper({ children, onRootClose, event: clickTrigger }) {
-      const ref = useRef();
-      useRootClose(ref, onRootClose, { clickTrigger });
-
-      return (
-        <div ref={ref} id="my-div">
-          {children}
-        </div>
-      );
-    }
-
-    it('should close when escape keyup', () => {
-      let spy = sinon.spy();
-      mount(
-        <Wrapper onRootClose={spy}>
-          <div id="my-div">hello there</div>
-        </Wrapper>,
-      );
-
-      expect(spy).to.not.have.been.called;
-
-      simulant.fire(document.body, 'keyup', { keyCode: escapeKeyCode });
-
-      expect(spy).to.have.been.calledOnce;
-
-      expect(spy.getCall(0).args.length).to.be.equal(1);
-      expect(spy.getCall(0).args[0].keyCode).to.be.equal(escapeKeyCode);
-      expect(spy.getCall(0).args[0].type).to.be.equal('keyup');
-    });
-
-    it('should close when inside another RootCloseWrapper', () => {
-      let outerSpy = sinon.spy();
-      let innerSpy = sinon.spy();
-
-      mount(
-        <Wrapper onRootClose={outerSpy}>
-          <div>
+      it('should close when escape keyup', () => {
+        let spy = sinon.spy();
+        mount(
+          <Wrapper onRootClose={spy}>
             <div id="my-div">hello there</div>
-            <Wrapper onRootClose={innerSpy}>
-              <div id="my-other-div">hello there</div>
-            </Wrapper>
-          </div>
-        </Wrapper>,
-      );
+          </Wrapper>,
+        );
 
-      simulant.fire(document.body, 'keyup', { keyCode: escapeKeyCode });
+        expect(spy).to.not.have.been.called;
 
-      // TODO: Update to match expectations.
-      // expect(outerSpy).to.have.not.been.called;
-      expect(innerSpy).to.have.been.calledOnce;
+        fire(document.body, 'keyup', { keyCode: escapeKeyCode });
 
-      expect(innerSpy.getCall(0).args.length).to.be.equal(1);
-      expect(innerSpy.getCall(0).args[0].keyCode).to.be.equal(escapeKeyCode);
-      expect(innerSpy.getCall(0).args[0].type).to.be.equal('keyup');
+        expect(spy).to.have.been.calledOnce;
+
+        expect(spy.getCall(0).args.length).to.be.equal(1);
+        expect(spy.getCall(0).args[0].keyCode).to.be.equal(escapeKeyCode);
+        expect(spy.getCall(0).args[0].type).to.be.equal('keyup');
+      });
+
+      it('should close when inside another RootCloseWrapper', () => {
+        let outerSpy = sinon.spy();
+        let innerSpy = sinon.spy();
+
+        mount(
+          <Wrapper onRootClose={outerSpy}>
+            <div>
+              <div id="my-div">hello there</div>
+              <Wrapper onRootClose={innerSpy}>
+                <div id="my-other-div">hello there</div>
+              </Wrapper>
+            </div>
+          </Wrapper>,
+        );
+
+        fire(document.body, 'keyup', { keyCode: escapeKeyCode });
+
+        // TODO: Update to match expectations.
+        // expect(outerSpy).to.have.not.been.called;
+        expect(innerSpy).to.have.been.calledOnce;
+
+        expect(innerSpy.getCall(0).args.length).to.be.equal(1);
+        expect(innerSpy.getCall(0).args[0].keyCode).to.be.equal(escapeKeyCode);
+        expect(innerSpy.getCall(0).args[0].type).to.be.equal('keyup');
+      });
     });
-  });
-});
+  }),
+);
